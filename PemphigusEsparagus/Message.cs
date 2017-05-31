f7fc63433c5bfc9b8b114d35445223069e9cdb4b
RedPhantom <mirpaa.itay@gmail.com>
2017-05-31 20:06:02 +0300

    Add project files.

---
+++ PemphigusEsparagus/Message.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+
+namespace PemphigusEsparagus
+{
+    public class Message
+    {
+        public long msgId { get; set; }
+        public string title { get; set; }
+        public string dateTime { get; set; }
+        public string author { get; set; }
+        public string body { get; set; }
+        public bool lastmsg { get; set; }
+
+        public enum renderType { XML, CSV }
+
+        /*
+         * 
+         * 
+         * Structure is:
+         * msgId    msgTitle    dd/mm/yyyy hh:mm    author  body
+         */
+        /// <summary>
+        /// Renders data in a csv-similar format, separating data by {||}. 
+        /// </summary>
+        /// <returns></returns>
+        public static string renderData(Message msg, renderType render = renderType.CSV)
+        {
+            if (render == renderType.CSV)
+            {
+                string separator = "{|}";
+                string data = msg.msgId + separator + msg.title + separator + msg.dateTime.ToString() + separator + msg.author + separator + msg.body;
+                return data;
+            }
+            else if (render == renderType.XML)
+            {
+                string _xml = "";
+                msg.body.Replace("\r\n", "<br>");
+                msg.body.Replace("\n", "<br>");
+                msg.body.Replace("\r", "<br>");
+
+                _xml += "<msg id=\"" + msg.msgId + "\">";
+                _xml += "<title>" + msg.title + "</title>";
+                _xml += "<datetime>" + msg.dateTime + "</datetime>";
+                _xml += "<author>" + msg.author + "</author>";
+                _xml += "<body>" + msg.body + "</body></msg>";
+                //if (msg.lastmsg) _xml += "</thread><thread>\n";
+                return _xml;
+            }
+            return null;
+        }
+
+        public static Message getData(long msgId)
+        {
+            Message msg = new Message();
+            WebClient wc = new WebClient();
+            wc.Encoding = System.Text.Encoding.UTF8;
+
+            string rawMsg = wc.DownloadString("http://www.ynet.co.il/NRFrame/Ext/App/TDG/TDGDisplayMessage/0,9734,2230-18160-" + msgId + "-2229-1,00.html");
+            //string _title = rawMsg.Substring(rawMsg.IndexOf("<font class=text18>"), rawMsg.IndexOf("</font><BR>") - rawMsg.IndexOf("<font class=text18>"));
+            int _titlesp = getStartPoint(rawMsg, "<font class=text18>");
+            int _titleep = getEndPoint(rawMsg, _titlesp, "</font><BR>");
+            string _title = rawMsg.Substring(_titlesp, _titleep).Replace("&nbsp;", "").Replace("נושא :", "");
+            rawMsg = cleanup("</font><BR>", rawMsg);
+            rawMsg = cleanup("<font class=text18>", rawMsg);
+
+
+            int _datesp = getStartPoint(rawMsg, "<font class=text12>");
+            int _dateep = getEndPoint(rawMsg, _datesp, "</font><BR>");
+            string _date = rawMsg.Substring(_datesp, _dateep).Replace("&nbsp;", "").Replace("תאריך :", "");
+            //_date.Replace("<font class=text12>", "").Replace("</font>", "");
+            rawMsg = cleanup("<font class=text12>", rawMsg);
+            rawMsg = cleanup("</font><BR>", rawMsg);
+
+
+            int _authorsp = getStartPoint(rawMsg, "<font class=text12>");
+            int _authorep = getEndPoint(rawMsg, _authorsp, "</font><BR>");
+            string _author = rawMsg.Substring(_authorsp, _authorep).Replace("&nbsp;", "").Replace("מחבר/ת :", "");
+            rawMsg = cleanup("<font class=text12>", rawMsg);
+            rawMsg = cleanup("</font><BR>", rawMsg);
+
+            rawMsg = cleanup("</script>", rawMsg);
+            int _bodysp = getStartPoint(rawMsg, "}\n	</script>");
+            int _bodyep = getEndPoint(rawMsg, _bodysp, "<BR></div>");
+
+            if (_bodyep < 0 || _bodysp < 0)
+            {
+                msg.body = "";
+            }
+            else
+            {
+                msg.body = rawMsg.Substring(_bodysp, _bodyep).Replace("&nbsp;", "").Replace("<BR>", "\n");
+            }
+
+
+
+            msg.title = _title;
+            msg.dateTime = _date.Substring(0, 8) + " " + _date.Substring(8, 5);
+            msg.author = _author;
+            msg.msgId = msgId;
+            // msg.body = _body;
+            return msg;
+        }
+
+        public static int getStartPoint(string data, string sp)
+        {
+            return data.IndexOf(sp) + sp.Length;
+        }
+
+        public static int getEndPoint(string data, int sp, string ep)
+        {
+            return data.IndexOf(ep) - sp;
+        }
+        public static string cleanup(string str, string data)
+        {
+            return data.Remove(data.IndexOf(str), str.Length);
+
+        }
+
+    }
+
+}
---
+++ PemphigusEsparagus/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PemphigusEsparagus
+{
+    class Program
+    {
+        const bool allinonefile = true;
+
+        static void Main(string[] args)
+        {
+            int pnum = 1; // page number
+            string totaldata = "";
+        Start:
+
+            //writeLog("Hello and welcome to pemphigus esparagus v1.0.");
+            WebClient wc = new WebClient();
+            if (!allinonefile)
+            {
+                totaldata = "";
+            }
+            bool startonnext = false;
+            string msg = ""; // temp message to work with
+            Regex regex = new Regex(Regex.Escape("o"));
+            Message tmpmsg = new PemphigusEsparagus.Message();
+
+
+            // download page, conv
+            Console.WriteLine("HTTP Request is active for page {0}.", pnum);
+
+            wc.Encoding = System.Text.Encoding.UTF8;
+            string page = wc.DownloadString(getPageURL(pnum));
+            Console.WriteLine("HTTP Request is complete.");
+            // end download
+
+            page = page.Replace("DMsg(MI", "eatshitynet"); // treat unwanted DMsg.
+            Console.WriteLine("Started processing page {0}", pnum);
+
+            Console.WriteLine(Regex.Matches(page, "DMsg").Count + " threads found.");
+            int bob = Regex.Matches(Regex.Escape(page), Regex.Escape("DMsg")).Count;
+            // for each message in the page
+            for (int i = 0; i < bob; i++)
+            {
+                if (startonnext)
+                {
+                    Console.WriteLine("Started thread (msg {0} in page).", i);
+                }
+                if (i == 0) totaldata += "<thread>";
+
+                int startIndex = page.IndexOf("DMsg(");
+                int endIndex = page.Substring(startIndex).IndexOf(");");
+                int len = endIndex - startIndex;
+                msg = page.Substring(startIndex, endIndex);
+                string msg2 = page.Substring(startIndex, 220);
+                tmpmsg = Message.getData(getMsgId(msg));
+
+                // writeLog(tmpmsg.msgId + "   " + tmpmsg.dateTime + "     " + tmpmsg.title + "     " + tmpmsg.author + "    " + tmpmsg.body);
+                totaldata += Message.renderData(tmpmsg, Message.renderType.XML);
+                Console.WriteLine("Msg {0} .", tmpmsg.msgId);
+                if (msg2.Contains("DEndT"))
+                {
+                    Console.WriteLine("Ended thread."); // last message in thread
+                    //tmpmsg.lastmsg = true;
+                    startonnext = true;
+                    totaldata += "</thread>";
+                }
+                else
+                {
+                    startonnext = false;
+                    tmpmsg.lastmsg = false;
+                }
+
+                page = page.Replace(msg, ""); // remove the used-up message.
+            }
+            if (!allinonefile)
+            {
+                System.IO.File.WriteAllText("ynetdata-pg." + pnum + ".xml", totaldata);
+                Console.WriteLine("Page {0} has been saved to disk.", pnum);
+            }
+            pnum++;
+            if (pnum <= 36) goto Start;
+            if (allinonefile)
+            {
+                System.IO.File.WriteAllText("compdata-pg." + pnum + ".xml", totaldata);
+            }
+        }
+
+        public static string getPageURL(int pagenum)
+        {
+            return ("http://www.ynet.co.il/home/0,7340,L-2230-18160-" + pagenum + ",00.html");
+        }
+
+        public static long getMsgId(string msg)
+        {
+            long msgId = long.Parse(msg.Substring(getStartPoint(msg, "DMsg("), 8)); // msgid is 8 chars long.
+            return msgId;
+        }
+
+        public static int getStartPoint(string data, string sp)
+        {
+            return data.IndexOf(sp) + sp.Length;
+        }
+
+
+    }
+}
